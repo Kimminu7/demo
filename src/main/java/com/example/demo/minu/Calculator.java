e446663915e0e768cd50e8034b99d8d86f2ff9d1
kimminu7 <rock9024@naver.com>
2025-03-12 21:49:15 +0900

    plus

--- src/main/java/com/example/demo/minu/Calculator.java
+++ src/main/java/com/example/demo/minu/Calculator.java
@@ -3,5 +3,6 @@ package com.example.demo.minu;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Calculator.main");
+        System.out.println("test");
     }
 }
