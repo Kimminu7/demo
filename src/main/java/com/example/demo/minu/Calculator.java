13d2645faf13dcb16cb7bf43660d9b51d28b159c
kimminu7 <rock9024@naver.com>
2025-03-12 20:16:43 +0900

    Git 추가

--- src/main/java/com/example/demo/minu/Calculator.java
+++ src/main/java/com/example/demo/minu/Calculator.java
@@ -4,5 +4,6 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Calculator.main");
         System.out.println("Hello world!");
+        System.out.println("Git");
     }
 }
